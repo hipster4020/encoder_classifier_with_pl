dcde66afee18e657d1fbc14773190bbd22d8314f
hipster4020 <hipster4020@gmail.com>
2022-08-31 05:55:02 +0000

    predict modified

--- src/predict.py
+++ src/predict.py
@@ -20,10 +20,10 @@ def main(cfg):
 
     # data load
     labels = cfg.DICT.labels
-    data = fm.load(cfg.PATH.test_data)
+    df = fm.load(cfg.PATH.test_data)
 
     data = tokenizer(
-        data.content.tolist(),
+        df.content.astype(str).values.tolist(),
         max_length=cfg.DATASETS.seq_len,
         padding="max_length",
         truncation=True,
@@ -34,8 +34,8 @@ def main(cfg):
     def sigmoid(x):
         return 1 / (1 + np.exp(-x))
 
+    pred = []
     with torch.no_grad():
-        pred = []
         src = data["input_ids"].cuda()
         src_mask = data["attention_mask"].cuda()
 
@@ -43,16 +43,19 @@ def main(cfg):
         outputs = outputs.cpu().numpy()
 
         sigmoid = sigmoid(outputs)
-        print(f"sigmoid : {sigmoid}")
+        np_where = [np.where(i > 0.8)[0].tolist() for i in sigmoid]
 
-    #     np_where = np.where(sigmoid > 0.8)
+        for v in np_where:
+            value = []
+            if not v:
+                value.append("etc")
+            else:
+                for i in v:
+                    value.append(labels[i])
+            pred.append(value)
 
-    #     if not np_where[0]:
-    #         pred = ["etc"]
-    #     else:
-    #         pred = [labels[int(i)] for i in np_where[0]]
-
-    #     print(f"pred : {pred}")
+    df["predict"] = pred
+    fm.save(cfg.PATH.check_data, df)
 
 
 if __name__ == "__main__":
